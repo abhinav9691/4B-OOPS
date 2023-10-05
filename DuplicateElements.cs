477316f618d849246d134c1173445365023e1140
Abhinav Kumar <kabhinav713@gmail.com>
2023-10-05 08:53:19 +0530

    [Abhinav]-adding duplication part

---
+++ DuplicateElements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4B_OOPS
+{
+    internal class DuplicateElements
+    {
+        public void getDuplicateElements()
+        {
+            int count = 0;
+            int[] arr = new int[5];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Total duplicate is :" + count);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4B_OOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1)Find Duplicates in an Array \n2)Unique Elements in an Array \n3)Frequency Of Elements in an Array \n4)Max And Min Element in Array \n5)Pattern1  \n6)Pattern2 \n7)Reverse String \n8)Sum of Digits of Number \n9)Sum oF each Row In Matrix");
+            int option = Convert.ToInt16(Console.ReadLine());
+            while (true)
+            {
+                switch (option)
+                {
+                    case 1:
+                        DuplicateElements duplicate = new DuplicateElements();
+                        duplicate.getDuplicateElements();
+                        break;
+                }
+            }
+        }
+                    
+    }
+}
